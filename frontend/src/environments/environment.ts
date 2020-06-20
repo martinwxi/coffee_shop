5fa3557ecccee878287b0842fda2b1b4427b6b5d
Martin Wang <martin@MartindeMacBook-Pro.local>
2020-06-20 20:04:57 +0100

    update some bugs

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'udacityproject.eu.auth0.com', // the auth0 domain prefix
+    url: 'udacityproject.eu', // the auth0 domain prefix
     audience: 'test', // the audience set for the auth0 app
     clientId: 'LIXFrbgkX7J5ShKN573mmfP00Ykau0qJ', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8080/login-results', // the base url of the running ionic application. 
